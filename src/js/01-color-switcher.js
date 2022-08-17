a99e76f8ad2c9f04457d13d7308da1fc8dd9a775
Roman Andrukhiv <andrukhivroman@gmail.com>
2022-08-17 19:38:23 +0300

    first task done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const refs = {
+  startBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
+};
 
+refs.startBtn.addEventListener('click', onChangeBgColorClick);
+refs.stopBtn.addEventListener('click', onStopChangeBgColor);
+
+let interval = null;
+const DELAY = 1000;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function onChangeBgColorClick() {
+  document.body.style.backgroundColor = getRandomHexColor();
+  interval = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, DELAY);
+  refs.startBtn.disabled = true;
+  refs.stopBtn.disabled = false;
+}
+
+function onStopChangeBgColor() {
+  refs.startBtn.disabled = false;
+  refs.stopBtn.disabled = true;
+  clearInterval(interval);
+}
