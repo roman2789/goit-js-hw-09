127fa31e101d8d90532d4ee0c345fc7fa5bc672f
Roman Andrukhiv <andrukhivroman@gmail.com>
2022-08-19 13:38:40 +0300

    task 3 done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,44 @@
+import Notiflix from 'notiflix';
+
+const refs = {
+  submitBtn: document.querySelector('button'),
+  form: document.querySelector('.form'),
+};
+refs.submitBtn.addEventListener('submit', createPromise);
+refs.form.addEventListener('submit', onInputForm);
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      } else {
+        reject(`❌ Rejected promise ${position} in ${delay}ms`);
+      }
+    }, delay);
+  });
+}
+let position = 0;
+
+function onInputForm(e) {
+  e.preventDefault();
+  const form = e.currentTarget;
+  let delay = form.elements.delay.value;
+  const amount = form.elements.amount.value;
+  const step = form.elements.step.value;
+
+  for (i = 1; i <= amount; i++) {
+    position = i;
+    createPromise(position, delay, step)
+      .then(message => {
+        Notiflix.Notify.success(message);
+      })
+      .catch(message => {
+        Notiflix.Notify.failure(message);
+      });
+    delay = Number(delay) + Number(step);
   }
+  refs.form.reset();
 }
